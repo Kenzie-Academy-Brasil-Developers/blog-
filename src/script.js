40f78394a5f5f6bc1dcae62e656d8b80a1759025
Ivanilton Neto <netijua@gmail.com>
2022-07-08 09:51:14 -0300

    Update script.js

---
+++ src/script.js
@@ -0,0 +1,27 @@
+import LayoutController from "./controllers/LayoutController.js";
+
+if(document.title === "Blog | M2 - Kenzie"){
+  window.location = "./src/pages/login.html"
+} 
+else if(document.title === "Blog - Login | M2 - Kenzier"){
+  LayoutController.loginPage()
+
+  const form = document.querySelector("form")
+  form.classList.add("slideFromBottom")
+} 
+else if(document.title === "Blog - Sign Up | M2 - Kenzier"){
+  LayoutController.registerPage()
+
+  const form = document.querySelector("form")
+  form.classList.add("slideFromBottom")
+} 
+else if(document.title === "Blog - Main | M2 - Kenzier"){
+  LayoutController.mainPage()
+
+  const pagination = document.querySelector(".main__pagination")
+  setTimeout(() => {
+    if(document.readyState === "complete" && pagination.innerHTML === ""){
+      location.reload()
+    }
+  }, 1000)
+}
