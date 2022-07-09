9c5181877c187cec488f12979e399bbcf1da8be2
Ivanilton Neto <netijua@gmail.com>
2022-07-09 13:06:51 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -18,10 +18,9 @@ else if(document.title === "Blog - Sign Up | M2 - Kenzier"){
 else if(document.title === "Blog - Main | M2 - Kenzier"){
   LayoutController.mainPage()
 
-  const pagination = document.querySelector(".main__pagination")
-  setTimeout(() => {
+  const pagination = document.querySelector(".main__pagination")  
     if(document.readyState === "complete" && pagination.innerHTML === ""){
       location.reload()
     }
-  }, 1000)
+  
 }
